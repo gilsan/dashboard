0ad4d71f77b1a421c609740de224b488883c9a10
gilsan <jhhan@allaboutsip.co.kr>
2018-07-02 05:03:25 +0900

    dashboard recipe

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,7 +1,7 @@
 export const Config = {
   production: true,
   config : {
-   apiKey: 'AIzaSyBvq04RntMzNf5zAmspkhMzv0aL0i0nVSI',
+
    authDomain: 'fir-app-b97d5.firebaseapp.com',
    databaseURL: 'https://fir-app-b97d5.firebaseio.com',
    projectId: 'fir-app-b97d5',
